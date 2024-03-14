b95afa08f8adc8d5d4a224846112ccae87aa0c51
ced-yxos <jean.cedricsanou@gmail.com>
2024-03-14 13:35:01 +0100

    update

--- decision.py
+++ decision.py
@@ -126,7 +126,6 @@ async def set_service_endpoint(data: dict):
     global redis_host, r
     redis_host = data["db_endpoint"] 
     r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
-
     print(f"Endpoints initialisation Done")
 
 if __name__ == "__main__":
